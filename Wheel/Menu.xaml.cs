622b48c6863bf8fb8e7de829090c6f78f7cbee0a
Den-ie <dpokosov@gmail.com>
2023-10-27 19:46:48 +0300

    Добавьте файлы проекта.

---
+++ Wheel/Menu.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Wheel
+{
+    /// <summary>
+    /// Логика взаимодействия для Menu.xaml
+    /// </summary>
+    public partial class Menu : Window
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void PlayWheel(object sender, RoutedEventArgs e)
+        {
+            MainWindow wheel = new MainWindow();
+            wheel.ShowDialog();
+        }
+
+        private void RulesOpen(object sender, RoutedEventArgs e)
+        {
+            Rules rule = new Rules();
+            rule.ShowDialog();
+        }
+
+        private void ExitGame(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Are you sure?", "Exit");
+           
+        }
+    }
+}
